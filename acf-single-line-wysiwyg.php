91fa4f66dcb016455cd98bdd5c1cd0f4d5a9626b
kingingkingdoms <37118190+kingingkingdoms@users.noreply.github.com>
2018-07-26 18:31:25 -0400

    Update acf-single-line-wysiwyg.php

--- acf-single-line-wysiwyg.php
+++ acf-single-line-wysiwyg.php
@@ -8,17 +8,17 @@ Author URI: https://millionairedigest.com/
 */
 
 /* Set the text domain */
-load_plugin_textdomain( 'acf-single_line_wysiwyg', false, dirname( plugin_basename(__FILE__) ) . '/lang/' ); 
+load_plugin_textdomain( 'acf-single-line-wysiwyg', false, dirname( plugin_basename(__FILE__) ) . '/lang/' ); 
 
 /* Include field type for ACF5 */
 function include_field_types_single_line_wysiwyg( $version ) {
-	include_once('acf-single_line_wysiwyg-v5.php');
+	include_once('acf-single-line-wysiwyg-v5.php');
 }
 add_action('acf/include_field_types', 'include_field_types_single_line_wysiwyg');
 
 /* Include field type for ACF4 */
 function register_fields_single_line_wysiwyg() {
-	include_once('acf-single_line_wysiwyg-v4.php');
+	include_once('acf-single-line-wysiwyg-v4.php');
 }
 add_action('acf/register_fields', 'register_fields_single_line_wysiwyg');
 
